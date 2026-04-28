5ca365ca41bf50f995c6611799d7fbadb9e1d27a
DrRaspec <leng79867@gmail.com>
2026-04-28 13:39:36 +0700

    refactor: inject repository into controllers via bindings
    - Update AddTransactionBinding to pass TransactionRepository
    - Update HomeBinding to pass TransactionRepository
    - Enables controllers to access data layer

--- lib/features/transaction/bindings/add_transaction_binding.dart
+++ lib/features/transaction/bindings/add_transaction_binding.dart
@@ -1,11 +1,16 @@
 import 'package:get/get.dart';
+import 'package:mini_wallet/features/transaction/domain/repositories/transaction_repository.dart';
 import 'package:mini_wallet/features/transaction/presentation/controllers/add_transaction_controller.dart';
 
 class AddTransactionBinding extends Bindings {
   @override
   void dependencies() {
     if (!Get.isRegistered<AddTransactionController>()) {
-      Get.lazyPut(() => AddTransactionController());
+      Get.lazyPut(
+        () => AddTransactionController(
+          repository: Get.find<TransactionRepository>(),
+        ),
+      );
     }
   }
 }
--- lib/features/transaction/bindings/home_binding.dart
+++ lib/features/transaction/bindings/home_binding.dart
@@ -1,11 +1,14 @@
 import 'package:get/instance_manager.dart';
+import 'package:mini_wallet/features/transaction/domain/repositories/transaction_repository.dart';
 import 'package:mini_wallet/features/transaction/presentation/controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
     if (!Get.isRegistered<HomeController>()) {
-      Get.lazyPut(() => HomeController());
+      Get.lazyPut(
+        () => HomeController(repository: Get.find<TransactionRepository>()),
+      );
     }
   }
 }
