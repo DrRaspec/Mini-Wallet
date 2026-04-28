16b4787afa4fe7874e6cb41e5ab46e763d76d964
DrRaspec <leng79867@gmail.com>
2026-04-28 16:02:33 +0700

    feat(ui): remove bottom navigation bar and update navigation flow

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -2,7 +2,6 @@ import 'package:go_router/go_router.dart';
 import 'package:mini_wallet/features/transaction/bindings/add_transaction_binding.dart';
 import 'package:mini_wallet/features/transaction/bindings/edit_transaction_binding.dart';
 import 'package:mini_wallet/features/transaction/bindings/home_binding.dart';
-import 'package:mini_wallet/features/shell/presentation/pages/app_shell_page.dart';
 import 'package:mini_wallet/core/widgets/binding_scope.dart';
 import 'package:mini_wallet/features/transaction/data/models/transaction_model.dart';
 import 'package:mini_wallet/features/transaction/presentation/pages/add_transaction_page.dart';
@@ -18,42 +17,69 @@ class AppRouter {
   static final routerConfig = GoRouter(
     initialLocation: RoutePaths.home,
     routes: [
-      ShellRoute(
-        builder: (context, state, child) {
-          return AppShellPage(child: child);
+      // ShellRoute(
+      //   builder: (context, state, child) {
+      //     return AppShellPage(child: child);
+      //   },
+      //   routes: [
+      //     GoRoute(
+      //       path: RoutePaths.home,
+      //       name: RouteNames.home,
+      //       builder: (context, state) {
+      //         return BindingScope(
+      //           binding: HomeBinding(),
+      //           child: const HomePage(),
+      //         );
+      //       },
+      //     ),
+      //     GoRoute(
+      //       path: RoutePaths.addTransaction,
+      //       name: RouteNames.addTransaction,
+      //       builder: (context, state) {
+      //         return BindingScope(
+      //           binding: AddTransactionBinding(),
+      //           child: const AddTransactionPage(),
+      //         );
+      //       },
+      //     ),
+      //     GoRoute(
+      //       path: RoutePaths.editTransaction,
+      //       name: RouteNames.editTransaction,
+      //       builder: (context, state) {
+      //         return BindingScope(
+      //           binding: EditTransactionBinding(),
+      //           child: const EditTransactionPage(),
+      //         );
+      //       },
+      //     ),
+      //   ],
+      // ),
+      GoRoute(
+        path: RoutePaths.home,
+        name: RouteNames.home,
+        builder: (context, state) {
+          return BindingScope(binding: HomeBinding(), child: const HomePage());
         },
-        routes: [
-          GoRoute(
-            path: RoutePaths.home,
-            name: RouteNames.home,
-            builder: (context, state) {
-              return BindingScope(
-                binding: HomeBinding(),
-                child: const HomePage(),
-              );
-            },
-          ),
-          GoRoute(
-            path: RoutePaths.addTransaction,
-            name: RouteNames.addTransaction,
-            builder: (context, state) {
-              return BindingScope(
-                binding: AddTransactionBinding(),
-                child: const AddTransactionPage(),
-              );
-            },
-          ),
-          GoRoute(
-            path: RoutePaths.editTransaction,
-            name: RouteNames.editTransaction,
-            builder: (context, state) {
-              return BindingScope(
-                binding: EditTransactionBinding(),
-                child: const EditTransactionPage(),
-              );
-            },
-          ),
-        ],
+      ),
+      GoRoute(
+        path: RoutePaths.addTransaction,
+        name: RouteNames.addTransaction,
+        builder: (context, state) {
+          return BindingScope(
+            binding: AddTransactionBinding(),
+            child: const AddTransactionPage(),
+          );
+        },
+      ),
+      GoRoute(
+        path: RoutePaths.editTransaction,
+        name: RouteNames.editTransaction,
+        builder: (context, state) {
+          return BindingScope(
+            binding: EditTransactionBinding(),
+            child: const EditTransactionPage(),
+          );
+        },
       ),
       GoRoute(
         path: RoutePaths.transactionDetails,
